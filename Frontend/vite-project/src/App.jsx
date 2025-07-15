2bf2999c213dbc7f832faf08d23f423729f7426f
1aovaibhav <vaibhav82004@gmail.com>
2025-07-15 14:42:06 +0530

    roadmap section

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
+import Roadmap from './components/Roadmap'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+      <div className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
           Vaibhav
       </div>
+      <Roadmap/>
+
     </>
   )
 }
 
-export default App
+export default App;
--- Frontend/vite-project/src/components/Roadmap.jsx
+++ Frontend/vite-project/src/components/Roadmap.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Roadmap = () => {
   return (
     <div>
-      
+      <div className='w-full h-[986px] bg-[#292C33] py-[100px] flex flex-col gap-[40px] justify-center items-center'>
+
+        <h1 className='w-full h-[64px] font-bold text-[48px] leading-[64px] text-center tracking-[4%]' style={{ color: "#F8FAFC", fontFamily: "'Gilroy', sans-serif" }}
+>How to unlock Career Accelerators?</h1>
+
+        <div className='w-full h-[554px]'>
+            <img src='/images/roadmap.svg' alt = "roadmap" className="h-full w-full"/>
+        </div>
+
+        <div className='flex flex-row gap-[40px]'>
+              <div className='w-[194px] h-[88px] bg-[#F8FAFC] rounded-md p-[12px] flex flex-col justify-center items-center'></div>
+        </div>
+
+      </div>
     </div>
   )
 }
