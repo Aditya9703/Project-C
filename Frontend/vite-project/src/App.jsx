e55e22c33572b7332daa57fa7ee6cdd66ec00168
Aditya9703 <kushwahaditya054@gmail.com>
2025-07-16 14:07:31 +0530

    hghgh

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import "./App.css";
 import Homepage from "./components/Homepage";
+import Instructor from "./components/Instructor";
 import Navbar from "./components/Navbar";
 import Roadmap from "./components/Roadmap";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Homepage />
+        <Instructor />
         <Roadmap />
       </Router>
     </>
