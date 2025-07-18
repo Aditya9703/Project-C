00aeb4fbc00234ae62067f45f383fed69373d923
anshul3112 <singhanshul3112@gmail.com>
2025-07-18 14:04:38 +0530

    navbar

--- Frontend/vite-project/src/components/Navbar.jsx
+++ Frontend/vite-project/src/components/Navbar.jsx
@@ -6,15 +6,23 @@ function Navbar() {
     const [dropDown2 , setDropDown2] = useState(false);
 
   return (
-    <div>
+    <div className='h-[96px] w-full border-b-1 px-[74px] py-[24px] 'style={{fontFamily: "'Gilroy' , sans-serif"}}>
           <nav className="navbar">
-            <div className="nav-links flex flex-row items-center font-25px justify-evenly">
+            <div className="flex w-[1312px] gap-[52px]">
+              <div className='w-[260px] h-[40px]'>
+                <img src="/images/LOGO.png" alt="" />
+              </div>
+              <div className='flex gap-[20px]'>
               <div>
-                <p>Fellowship program</p>
+                <div className=' w-[148px] h-[24px]'>
+                <p className='text-center  font-normal text-[16px]/[24px]'>Fellowship program</p>
+                </div>
+                <div>
               <Link to="/">Launch tech Career</Link>
               </div>
+              </div>
               <div>
-                <p>Renaissance</p>
+                <p className='text-center'>Renaissance</p>
               <Link to="/">Interview Preperation</Link>
               </div>
               <div className='relative'>
@@ -40,7 +48,7 @@ function Navbar() {
               <div className='relative'>
                 <div>
                     <button onClick={() => {setDropDown2(!dropDown2)}}>
-                        Certification Courses
+                        Resources
                     </button>
                 </div>
                     {dropDown2 &&
@@ -56,7 +64,7 @@ function Navbar() {
                     )
                     }
               </div>
-
+              </div>
             </div>
           </nav>
     </div>
